0cfaac725f20fd0f22a3ae391d9936aee7e0c52f
Charles Cressey <cmcressey@gmail.com>
2020-02-14 16:14:52 -0700

    updated index

--- index.js
+++ index.js
@@ -1,105 +1,64 @@
-const axios = require("axios");
-const inquirer = require("inquirer");
-// const PDFDocument = require("pdfkit");
 const fs = require("fs");
-const PDFDocument = require('pdfkit');
-// const fs = require('fs');
+const path = require("path");
+const inquirer = require("inquirer");
+const open = require("open");
+const convertFactory = require("electron-html-to");
+const api = require("./api");
+const generateHTML = require("./generateHTML");
 
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
 
+  {
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "pink"]
+  }
+];
 
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// ----------------------- makes call to GitHub api
-// use axios to make a call to github
-// install axios
+function init() {
+  inquirer.prompt(questions).then(({ github, color }) => {
+    console.log("Searching...");
 
-const queryUrl = `https://api.github.com/users/7thact`;
+    api
+      .getUser(github)
+      .then(response =>
+        api.getTotalStars(github).then(stars => {
+          return generateHTML({
+            stars,
+            color,
+            ...response.data
+          });
+        })
+      )
+      .then(html => {
+        const conversion = convertFactory({
+          converterPath: convertFactory.converters.PDF
+        });
 
-axios.get(queryUrl).then(function (res) {
-    console.log(res);
+        conversion({ html }, function(err, result) {
+          if (err) {
+            return console.error(err);
+          }
 
-    // ----------------------- then asks user for fav color
-    // prompt the user for a color
-    // install inquirer
-    inquirer.prompt([{
-        name: 'color',
-        message: "What is your favorite color"
-    }]).then(answers => {
-        // console.log('oh so your fave color is ' + answers.color + '?')
-        // -----------------------  then generates a pdf of the GithHub profile
-        // google
+          result.stream.pipe(
+            fs.createWriteStream(path.join(__dirname, "resume.pdf"))
+          );
+          conversion.kill();
+        });
 
-        // Create a document
-        const doc = new PDFDocument();
+        open(path.join(process.cwd(), "resume.pdf"));
+      });
+  });
+}
 
-        // Pipe its output somewhere, like to a file or HTTP response
-        // See below for browser usage
-        doc.pipe(fs.createWriteStream('output.pdf'));
-
-        // Embed a font, set the font size, and render some text
-        doc
-            // .font('fonts/PalatinoBold.ttf')
-            .fontSize(25)
-            .text(`
-                userName: ${res.data.name}
-
-            `, 100, 100);
-
-        // Add an image, constrain it to a given size, and center it vertically and horizontally
-        // doc.image(res.data.avatar_url, {
-        //     fit: [250, 300],
-        //     align: 'center',
-        //     valign: 'center'
-        // });
-
-        // Add another page
-        // doc
-        //     .addPage()
-        //     .fontSize(25)
-        //     .text('Here is some vector graphics...', 100, 100);
-
-        // Draw a triangle
-        doc
-            .save()
-            .moveTo(100, 150)
-            .lineTo(100, 250)
-            .lineTo(200, 250)
-            .fill('#FF3300');
-
-        // Apply some transforms and render an SVG path with the 'even-odd' fill rule
-        doc
-            .scale(0.6)
-            .translate(470, -380)
-            .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
-            .fill('red', 'even-odd')
-            .restore();
-
-        // Add some text with annotations
-        doc
-            .addPage()
-            .fillColor('blue')
-            .text('Here is a link!', 100, 100)
-            .underline(100, 100, 160, 27, { color: '#0000FF' })
-            .link(100, 100, 160, 27, 'http://google.com/');
-
-        // Finalize PDF file
-        doc.end();
-
-    });
-
-
-})
-
-
-
-
-
-// const userName = 'per'
-// const template = `
-// <html>
-// <body>
-// <ul>
-// <li>${userName}</li>
-// </ul>
-// </body>
-// </html>
-// `
+init();
